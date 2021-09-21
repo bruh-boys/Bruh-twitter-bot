718c5fc7cb7d4a3641284232b2de459d195224bf
jheremy <jheremy808@gmail.com>
2021-09-20 23:06:27 -0400

    Listo para desplegar

--- src/index.ts
+++ src/index.ts
@@ -26,32 +26,62 @@ const tweet_write = new Twitter({
     access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 })
 
-const tuitxD = 'Bruh Sound Effect #2 - 22.690.690 views'
+const mensaje_xD = `
+    
+----------------------------------------------------------------------------------------
+/////////////       //////////////         //////////////    ||             ///      ///
+///        ||/     ////        ////       ////               ||              ///    ///
+///        ||/    ////          ////      ///                ||               ///  ///
+/////////////    ||||            ||||     //                 ||                //////
+//////////       ||||            ||||     //                 ||/////////        //// 
+///    ////      ////            ////     ///                ||        //       ///
+///     ////      ////          ////      ////               ||         ||     ///
+///      ////      ////////////////        //////////////    ||         ||    ///
+----------------------------------------------------------------------------------------
+
+`
+
+
 
 // Lo unico que importa:
 
-const hoy = () => {
-    var dia = new Date()
-    return dia
+const YouTube = require('youtube-node');
 
+const youTube = new YouTube();
+
+youTube.setKey(process.env.GOOGLE_API_KEY);
+
+
+const twit_opa_opa = () =>{
+    youTube.getById('2ZIpFytCSVc', function(error: any, result: any) {
+        if (error) {
+          console.log(error);
+        }
+        else {
+            tweet_write.post('statuses/update', {status: `The video "Bruh Sound Effect # 2" currently has ${result.items[0].statistics.viewCount} views on Youtube.. (https://www.youtube.com/watch?v=2ZIpFytCSVc)`},  function(error, tweet, response) {
+                if(error) throw error;
+                console.log(mensaje_xD); 
+            })
+        }
+    })
 }
-
+ 
 
 const publicar = () =>{
-    setInterval(() => {
-        tweet_write.post('statuses/update', {status: `Estoy en face de desarrollo asi que por ahora solo digo la hora. Son las: ${hoy().getHours()}:${hoy().getMinutes()}:${hoy().getSeconds()} xD`},  function(error, tweet, response) {
-            if(error) throw error;
-            console.log(tweet);
-            console.log('si, ya son'); 
-        })
-    }, 300000);
-    // 5mn: 300000,15 mn: 900000, media hora: 1800000 x 2
-}
 
+        twit_opa_opa()
+
+    setInterval(() => {
+
+        twit_opa_opa()
+
+    }, 3600000);
+    // 5mn: 300000, 15 mn: 900000, media hora: 1800000, hora: 3600000
+}
 
 app.listen(port, ()=>{
     console.log(`Online on port ${port}`)
-
-    console.log(`${hoy().getHours()}:${hoy().getMinutes()}:${hoy().getSeconds()}:${hoy().getUTCMilliseconds()}`);
+    console.log();
+    
     publicar()
 })
